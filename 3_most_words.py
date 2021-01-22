c8a6e7df043a10069b1276d9afb150738aefba7b
shklyarbogdan <shklyarbogdan@gmail.com>
2021-01-22 18:11:52 +0200

    3_wd_counter_finished

--- 3_most_words.py
+++ 3_most_words.py
@@ -1,16 +1,21 @@
 import re
 from collections import Counter
 
-text = "Seba CJ2 are based around concept of a boot with integrated padding. \
-        This brings benefits of reduced weight and increased precision of following your movements by skate.\
-        While CJ Carbon are made, as name suggests, with carbon fibre shell, CJ 2 uses a composite material equivalent.\
-        As a result, skate is a bit heavier than its more high end counterpart, but also a bit more flexible. \
-        This makes the skate feeling less rigid out of the box and will appeal to skaters who prefer a bit\
-        more ankle freedom as well as skates that bend more on tricks. It’s worth to note that while CJ\
-        2 are still a nice boot for custom setups with big wheels, they won’t offer the same level of energy\
-        transfer efficiency as carbon made ones. If you are after a boot to attach your Wizard frames to – CJ Carbon are \
-        still the top choice. On the plus side – composite shell means that skate costs less overall."
-words = re.findall(r'\w+', text)
-c = Counter(words)
+words_list = []
 
-print (c.most_common(3))
+def words_counter(text):
+    words = re.findall(r'\w+', text)
+    for word in words:
+        words_list.append(word.lower())
+
+    c = Counter(words_list)
+    most_common_words = (dict(c.most_common(3))).keys()
+    
+    if len(c) >= 3:
+        print("Most common words:",most_common_words)
+    else:
+        words_list.clear()
+        print("less than 3", words_list)
+
+text = input("Enter text: ")
+words_counter(text)
